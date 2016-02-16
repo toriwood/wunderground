0195d5fde74ae5854b51fba40cf59df3eead4eb5
Tori <victoria.ashley.wood@gmail.com>
2016-02-15 20:52:12 -0500

    updated application.html.erb to have conditional links based on current_user's logged in or out status

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,6 @@ class HomeController < ApplicationController
    	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC )
   	@states.sort!
 
-    response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/FL/Jacksonville.json")["current_observation"]
   	if params[:state] != nil && params[:city] != nil
   		params[:city].gsub! " ", "_"
   		response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/#{params[:state]}/#{params[:city]}.json")["current_observation"]
