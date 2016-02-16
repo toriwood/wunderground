70b0597c3ea30e85d76aeefd673a7c2e6185b843
Tori <victoria.ashley.wood@gmail.com>
2016-02-15 20:32:19 -0500

    trying to narrow down heroku problems

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,13 +12,13 @@ class HomeController < ApplicationController
 
    	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC )
   	@states.sort!
-  	if params[:state] != nil && params[:city] != nil
-  		params[:city].gsub! " ", "_"
-  		response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/#{params[:state]}/#{params[:city]}.json")["current_observation"]
-  	else
-  		# @current_city.gsub! " ", "_"
+  	# if params[:state] != nil && params[:city] != nil
+  	# 	params[:city].gsub! " ", "_"
+  	# 	response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/#{params[:state]}/#{params[:city]}.json")["current_observation"]
+  	# else
+  	# 	# @current_city.gsub! " ", "_"
   		response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/FL/Jacksonville.json")["current_observation"]
-  	end
+  	# end
   	#location, temp_f, temp_c, weather_icon, weather_word, forecast_link, feels_like
   	
   	@location = response["display_location"]["city"]
