a80285167a22f49d26baa2297b133c1f63c24771
Tori <victoria.ashley.wood@gmail.com>
2016-02-15 20:45:30 -0500

    fixed api key in heroku and adding things back into index action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,14 +14,14 @@ class HomeController < ApplicationController
   	@states.sort!
 
     response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/FL/Jacksonville.json")["current_observation"]
-  # 	# if params[:state] != nil && params[:city] != nil
-  # 	# 	params[:city].gsub! " ", "_"
-  # 	# 	response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/#{params[:state]}/#{params[:city]}.json")["current_observation"]
-  # 	# else
-  # 	# 	# @current_city.gsub! " ", "_"
-  # 		response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/FL/Jacksonville.json")["current_observation"]
-  # 	# end
-  # 	#location, temp_f, temp_c, weather_icon, weather_word, forecast_link, feels_like
+  	if params[:state] != nil && params[:city] != nil
+  		params[:city].gsub! " ", "_"
+  		response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/#{params[:state]}/#{params[:city]}.json")["current_observation"]
+  	else
+  		# @current_city.gsub! " ", "_"
+  		response = HTTParty.get("http://api.wunderground.com/api/#{ENV["wunderground_api_key"]}/conditions/q/FL/Jacksonville.json")["current_observation"]
+  	end
+  	#location, temp_f, temp_c, weather_icon, weather_word, forecast_link, feels_like
 
   	@location = response["display_location"]["city"]
   	@temp_f = response["temp_f"]
